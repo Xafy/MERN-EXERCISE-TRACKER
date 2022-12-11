34773d5221fb056e9a88f565c3a5537c3dab6d97
Xafy <57871519+Xafy@users.noreply.github.com>
2022-12-11 02:36:02 +0200

    Init the project

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import Createuser from "./components/create-user.component";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <Navbar/>
+        <br/>
+        <Routes>
+          <Route path="/" exact element={<ExercisesList/>}/>
+          <Route path="/edit/:id" exact element = {<EditExercise/>}/>
+          <Route path="/create" exact element = {<CreateExercise/>}/>
+          <Route path="/user" exact element = {<Createuser/>}/>        
+      </Routes>        
+      </div>
+
+
+    </Router>    
+  );
+}
+
+export default App;
